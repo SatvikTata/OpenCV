d7dfb376be2a2a21b541feaeb8ea40fda1317cc8
Satvik <iamsatvik77@gmail.com>
2023-01-20 21:38:17 +0530

    Reading images and video

--- read.py
+++ read.py
@@ -1,6 +1,17 @@
 import cv2 as cv
 
-img = cv.imread('images/cat_large.jpg')
-cv.imshow('Cat',img)
+#img = cv.imread('images/cat_large.jpg')
+#cv.imshow('Cat',img)
+#cv.waitKey(0)
 
-cv.waitKey(0)
+capture=cv.VideoCapture('videos/dog.mp4')
+
+while True:
+    isTrue,frame=capture.read()
+    cv.imshow('video', frame)
+
+    if cv.waitKey(20) & 0xFF==ord('d'):
+        break
+
+capture.release()
+cv.destroyAllWindows()
