a8032e89e9084bc09496917b7ba1e3be3d8e4dbd
Satvik <iamsatvik77@gmail.com>
2023-01-21 00:52:19 +0530

    Resizing and Rescaling the media file

---
+++ rescale.py
@@ -0,0 +1,34 @@
+import cv2 as cv
+
+#function for resizing
+def resize(frame,scale=0.45):
+    height=int(frame.shape[0]*scale)
+    width=int(frame.shape[1]*scale)
+
+    dimensions=(width,height)
+
+    return cv.resize(frame,dimensions,interpolation=cv.INTER_AREA)
+
+#image part
+image=cv.imread('images/cat_large.jpg')
+resized_image=resize(image)
+cv.imshow('image',image)
+cv.imshow('New Image',resized_image)
+cv.waitKey(0)
+
+
+#video part
+capture=cv.VideoCapture('videos/dog.mp4')
+while True:
+    isTrue, frame=capture.read()
+    frame_resized=resize(frame)
+    #cv.imshow('Video',frame)
+    cv.imshow("Resized Video",frame_resized)
+
+    if(cv.waitKey(20) & 0xFF==ord('d')):
+        break
+
+capture.release()
+cv.destroyAllWindows()
+
+cv.waitKey(0)
